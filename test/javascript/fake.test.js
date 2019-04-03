76f803b25ea38c9a2bc4a7e4b472e32ed74b95d4
FrankCardillo <facardillo91@gmail.com>
2019-04-03 16:19:12 -0400

    adding enzyme/jest for react testing

---
+++ test/javascript/fake.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+import LandingPage from 'exampleApp/components/landingPage';
+
+test('rendered component', () => {
+  const wrapper = shallow(<LandingPage
+	/>);
+	const myText = wrapper.render().text()
+  expect(myText).to.equal('Hello World');
+});
